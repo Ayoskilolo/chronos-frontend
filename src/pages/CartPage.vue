ccc4c5a1f5c3c70dc7e1c94aa2ab4d8066e35c74
Ayoskilolo <ayoafo82@gmail.com>
2022-10-14 23:28:32 +0100

    wahalat

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -41,42 +41,41 @@
     </q-drawer>
 
     <body>
-      <div class="Cart-Container">
+      <div class="Cart-Container"> 
         <div class="Header">
           <h3 class="Heading">Shopping Cart</h3>
-          <h5 class="Action">Remove all</h5>
         </div>
 
+        <ul>
+          <li v-for="item in order" :key="item.id">
         <div class="Cart-Items">
           <div class="image-box">
-            <img src="images/apple.png" style="height: 120px" />
+            <img :src=path style="height: 120px" />
           </div>
 
           <div class="about">
-            <h1 class="title">Apple Juice</h1>
+            <h1 class="title">{{item.item}}</h1>
             <h3 class="subtitle">250ml</h3>
-            <img src="images/veg.png" style="height: 30px" />
+            <!-- <img src="images/veg.png" style="height: 30px" /> -->
           </div>
 
           <div class="counter">
-            <div class="btn">+</div>
-            <div class="count">2</div>
-            <div class="btn">-</div>
+            <div class="count">{{item.quantity}}</div>
           </div>
 
           <div class="prices">
-            <div class="amount">$2.99</div>
-            <div class="save"><u>Save for later</u></div>
-            <div class="remove"><u>Remove</u></div>
+            <div class="amount">${{ item.price }}</div>
           </div>
         </div>
+      </li>
+      </ul>
 
         <hr />
         <div class="checkout">
           <div class="total">
             <div>
               <div class="Subtotal">Sub-Total</div>
-              <div class="items">2 items</div>
+              <div class="items">{{order.length}} items</div>
             </div>
             <div class="total-amount">$6.18</div>
           </div>
@@ -87,13 +86,17 @@
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue';
+<script setup>
 
-export default defineComponent({
-  // data () {
-  // }
-});
+import { useOrderStore } from 'src/stores/order';
+
+const OrderStore = useOrderStore();
+const order = OrderStore.order;
+const userName = localStorage.userName;
+
+const path = `@/assets/img/${item.item}.png`
+
+
 </script>
 
 <style>
